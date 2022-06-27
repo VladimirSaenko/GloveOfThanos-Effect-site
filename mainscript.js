89254c31a1d39194cdd58a686ee73443fba7d5a4
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-06-27 13:53:56 +0300

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -12,7 +12,7 @@ for(let i = 1; i < 400; i++) {
     blocks[i].style.animationDelay = duration + 's';
 }
 
-setTimeout(function() {
-    section.classList.add('active');
-    location.reload();
-}, 15000);
+// setTimeout(function() {
+//     section.classList.add('active');
+//     location.reload();
+// }, 15000);
